9025beb818b53a64b370d0092c6cef573c27fb0d
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2023-09-01 19:01:39 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,14 +20,24 @@ function Img() {
 
 function BigParagraph() {
   return (
-    <h1 className="fw-700 margin-left-right-1 fs-22px">
+    <h1
+      style={{
+        color: "hsl(218, 44%, 22%)",
+      }}
+      className="fw-700 margin-left-right-1 fs-22px"
+    >
       Improve your front-end skills by building projects
     </h1>
   );
 }
 function SmallParagraph() {
   return (
-    <p className="margin-left-right-1 magin-bottom-1rem fs-15px width-18p2-rem  ">
+    <p
+      style={{
+        color: "hsl(220, 15%, 55%)",
+      }}
+      className="margin-left-right-1 magin-bottom-1rem fs-15px width-18p2-rem  "
+    >
       Scan the QR code to visit Frontend Mentor and take your coding skills to
       the next level
     </p>
